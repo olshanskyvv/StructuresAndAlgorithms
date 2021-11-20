1d66476115a044e3acadaaf8eb1712ed4873f77d
olshanskyvv <90987935+olshanskyvv@users.noreply.github.com>
2021-11-20 20:25:48 +0300

    Create BinSearchTree.cpp

---
+++ BinSearchTree.cpp
@@ -0,0 +1,54 @@
+struct Node {
+  int value = -1;
+  Node* left = nullptr;
+  Node* right = nullptr;
+};
+
+struct BinTree {
+  Node* root = nullptr;
+};
+
+void Insert(Node*& node, int value) {
+  if (node == nullptr) {
+    node = new Node{value};
+    return;
+  }
+  if (value == node->value) {
+    return;
+  }
+  if (value >= node->value) {
+    Insert(node->right, value);
+  } else {
+    Insert(node->left, value);
+  }
+}
+
+void Insert(BinTree& tree, int value) {
+  Insert(tree.root, value);
+}
+
+void OrderWalk(Node* node) {
+  if (node == nullptr) {
+    return;
+  }
+  OrderWalk(node->left);
+  cout << node->value << " ";
+  OrderWalk(node->right);
+}
+
+void OrderWalk(BinTree& tree, int& counter) {
+  OrderWalk(tree.root, counter);
+ }
+
+void Clear(Node* node) {
+  if (node == nullptr) {
+    return;
+  }
+  Clear(node->left);
+  Clear(node->right);
+  delete node;
+}
+
+void Clear(BinTree& tree) { 
+  Clear(tree.root);
+}
